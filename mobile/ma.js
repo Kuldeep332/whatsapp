dd5ccb1ef3780383b803d98f32453a00eb50d97b
kuldeep <kuldeepdhakad152@gmail.com>
2023-08-28 10:20:37 +0530

    first commit

---
+++ mobile/ma.js
@@ -0,0 +1,232 @@
+
+             var arr=[{
+	name:"New community",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"8:12:3"
+},
+{
+	name:"Announcement",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"today"
+},
+{
+	name:"All aboutevent",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+
+{
+	name:"vikash dhakad",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"alo patiap",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"grisha guptha",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"rakesh kumar",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"bhata yadav",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"julli kumari",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"sanjeet kumar",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"vicky dhothe",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"shane raza",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"abhishek yadav",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"kuldeep dhakad",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"vikash dhakad",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"vikash dhakd",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"vikash dhakad",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"vikash dhakad",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday dhakad"
+},
+{
+	name:"vikash dhakad",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},{
+	name:"vikash dhakad",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"vikash dhakad",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"vikash hakad",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},{
+	name:"vikash dhakad",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"vikash",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+{
+	name:"vikash dhakad",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},{
+	name:"vikash",
+	img:"https://images.unsplash.com/photo-1604514628550-37477afdf4e3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=327&q=80",
+	status:"yesterday"
+},
+
+]
+var clutter=""
+arr.forEach(function(elen,index){
+	clutter+=`
+	<div id="op">
+	<img src="${elen.img}" alt="">
+<h1 id="aa">${elen.name}</h1>
+<h4>${elen.status}</h4>
+</div>
+	
+	`
+})
+
+document.querySelector("#nav2").innerHTML=clutter
+var d=0
+var h5=document.querySelector("h5").addEventListener("click",function(){
+	if(d==0){
+	
+	document.querySelector("#nav2").style.display="initial"
+
+	d++
+	}
+	else{
+		document.querySelector("#nav2").style.display="none"
+		d--
+
+
+	}
+})
+// document.querySelector("#nav2").innerHTML=clutter
+var d12=0
+var h5=document.querySelector("#car").addEventListener("click",function(){
+	if(d12==0){
+	
+	document.querySelector("#nav2").style.display="initial"
+
+	d12++
+	}
+	else{
+		document.querySelector("#nav2").style.display="none"
+		document.querySelector("#hello").style.display="none"
+
+		d12--
+
+
+	}
+})
+var d13=0
+var h5=document.querySelector("#car1").addEventListener("click",function(){
+	if(d13==0){
+	
+	document.querySelector("#hello").style.display="initial"
+
+	d13++
+	}
+	else{
+		document.querySelector("#hello").style.display="none"
+		d13--
+
+
+	}
+})
+var d14=0
+var h5=document.querySelector("#uu").addEventListener("click",function(){
+	if(d14==0){
+	
+	document.querySelector("#status").style.display="initial"
+
+	d14++
+	}
+	else{
+		document.querySelector("#status").style.display="none"
+		d14--
+
+
+	}
+})
+
+
+
+
+// document.querySelector("#nav2").innerHTML=clutter
+var d1=0
+var h5=document.querySelector("#aa").addEventListener("click",function(){{
+	
+	document.querySelector("#chat").style.display="initial"
+	document.querySelector("#nav").style.display="none"
+	
+	
+	}
+	
+		
+
+
+	
+})
+document.querySelector("h6").addEventListener("click",function(){
+	document.querySelector("#chat").style.display="none"
+	document.querySelector("#nav").style.display="initial"
+
+
+})
